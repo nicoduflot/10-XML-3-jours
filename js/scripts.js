501295c5b572091cde3f83f42be77ee5cd4737c0
unknown <nduflot@dawan.fr>
2023-03-01 09:29:25 +0100

    ajout du listing des attributs

--- js/scripts.js
+++ js/scripts.js
@@ -33,10 +33,28 @@ function cTN(texte){
     return document.createTextNode(texte);
 }
 
-function parseXML(xmlData, html = '') {
-    /* la fonction est récursive et pourra lire tous les niveaux d'un XML */
+function parseXML(xmlData, html = '', userOptions = null) {
+    /* 
+    /!\ ATTENTION /!\
+    Cette fonction ne marche pour l'instant pas si des dtd sont définies en interne ou en externe
+
+    la fonction est récursive et pourra lire tous les niveaux d'un XML 
+    */
     /* le XML sera affiché sous la forme d'une liste html */
     html = cEO('ul');
+    /* 
+    options de l'affichage des attributs des éléments XML 
+    soit demandées par l'utilisateur soit par défaut, 
+    les classes ne fonctionnants qu'avec 
+    l'utilisation de bootstrap 5.2 minimum
+    */
+    let options = userOptions || {
+        style: `
+            font-style:italic;
+            font-weight:bold;
+            list-style-type: none`, 
+        class: 'alert alert-primary p-0  ps-2 pe-2 mb-0'
+    };
     /* on  parcours tous les noeuds enfant de la racine */
     xmlData.childNodes.forEach(element => {
         /* si le noeud n'est pas un noeud texte ou un commentaire */
@@ -44,12 +62,33 @@ function parseXML(xmlData, html = '') {
             /* si le noeud a des enfants, il s'agit d'une sous-liste */
             if (element.children.length !== 0) {
                 let li = cEO('li');
-                li.appendChild(cTN(`${element.nodeName} :`))
+                li.appendChild(cTN(`${element.nodeName} :`)); 
+                if(element.attributes.length > 0){
+                    
+                    let ulAttr = cEO('ul', options);
+                    for(attr of element.attributes){
+                        let liAttr = cEO('li');
+                        let elemAttr = cTN(`attr : ${attr['name']} => ${attr['value']}`)
+                        liAttr.appendChild(elemAttr);
+                        ulAttr.appendChild(liAttr);
+                    }
+                    li.appendChild(ulAttr);
+                }
                 html.appendChild(li);
             } else {
                 /* si pas d'enfant, c'est un élément de liste */
                 let li = cEO('li');
-                li.appendChild(cTN(`${element.nodeName} : ${element.innerHTML}`))
+                li.appendChild(cTN(`${element.nodeName} : ${element.innerHTML}`));
+                if(element.attributes.length > 0){
+                    let ulAttr = cEO('ul', options);
+                    for(attr of element.attributes){
+                        let liAttr = cEO('li');
+                        let elemAttr = cTN(`attr : ${attr['name']} => ${attr['value']}`)
+                        liAttr.appendChild(elemAttr);
+                        ulAttr.appendChild(liAttr);
+                    }
+                    li.appendChild(ulAttr);
+                }
                 html.appendChild(li);
             }
             if (element.children.length !== 0) {
